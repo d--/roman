ea00bc93db117e1181ecdf1745ab6232d6f46833
d-- <d--@users.noreply.github.com>
2016-08-12 15:33:37 -0400

    prepare to add roman calculator errors

---
+++ src/rome_calc_err.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <string.h>
+#include "rome_calc_err.h"
+
+static RomeCalcError rome_calc_errors[] = {
+    { ROME_CALC_E_SUCCESS },
+    { ROME_CALC_E_NULL_BUFFER },
+    { ROME_CALC_E_INPUT_NULL },
+    { ROME_CALC_E_BUFFER_NOT_EMPTY },
+    { ROME_CALC_E_EMPTY_STRING },
+    { ROME_CALC_E_UNKNOWN }
+};
+
+void rome_calc_error(RomeCalcError **out, int code)
+{
+    if (*out == NULL) {
+        return;
+    }
+
+    RomeCalcError *next;
+    for (next = rome_calc_errors; next->code != ROME_CALC_E_UNKNOWN; next++) {
+        if (code == next->code) {
+            break;
+        }
+    }
+
+    memcpy(*out, next, sizeof(RomeCalcError));
+}
---
+++ src/rome_calc_err.h
@@ -0,0 +1,20 @@
+#ifndef ROME_CALC_ERR_H
+#define ROME_CALC_ERR_H
+
+typedef struct {
+    int code;
+    char *message;
+} RomeCalcError;
+
+enum {
+    ROME_CALC_E_SUCCESS,
+    ROME_CALC_E_NULL_BUFFER,
+    ROME_CALC_E_INPUT_NULL,
+    ROME_CALC_E_BUFFER_NOT_EMPTY,
+    ROME_CALC_E_EMPTY_STRING,
+    ROME_CALC_E_UNKNOWN
+};
+
+extern void rome_calc_error(RomeCalcError **out, int code);
+
+#endif /* ROME_CALC_ERR_H */
