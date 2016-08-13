622b4bee75316241d35f2aa9efe885d3e0143a74
d-- <d--@users.noreply.github.com>
2016-08-12 20:53:46 -0400

    extract rome_calc arithmetic input validation

--- src/rome_calc.c
+++ src/rome_calc.c
@@ -4,30 +4,40 @@
 #include "roman.h"
 
 enum {
-    ROME_ADD_OP,
-    ROME_SUBTRACT_OP
+    ROME_CALC_ADD_OP,
+    ROME_CALC_SUBTRACT_OP
 };
 
-static void arithmetic(char const *a, char const *b, char *buffer,
-        RomeCalcError *err, int operation) {
-
+static int arithmetic_input_validation(char const *a, char const *b,
+        char *buffer, RomeCalcError *err)
+{
     if (buffer == NULL) {
         rome_calc_error(err, ROME_CALC_E_NULL_BUFFER);
-        return;
+        return 0;
     }
 
     if (buffer[0] != '\0') {
         rome_calc_error(err, ROME_CALC_E_BUFFER_NOT_EMPTY);
-        return;
+        return 0;
     }
 
     if (a == NULL || b == NULL) {
         rome_calc_error(err, ROME_CALC_E_INPUT_NULL);
-        return;
+        return 0;
     }
 
     if (a[0] == '\0' || b[0] == '\0') {
         rome_calc_error(err, ROME_CALC_E_EMPTY_STRING);
+        return 0;
+    }
+
+    return 1;
+}
+
+static void arithmetic(char const *a, char const *b, char *buffer,
+        RomeCalcError *err, int operation) {
+
+    if (!arithmetic_input_validation(a, b, buffer, err)) {
         return;
     }
 
@@ -45,7 +55,7 @@ static void arithmetic(char const *a, char const *b, char *buffer,
         return;
     }
 
-    if (operation == ROME_ADD_OP) {
+    if (operation == ROME_CALC_ADD_OP) {
         to_roman(a_num + b_num, buffer, &roman_err); 
     } else {
         to_roman(a_num - b_num, buffer, &roman_err);
@@ -62,11 +72,11 @@ static void arithmetic(char const *a, char const *b, char *buffer,
 void rome_add(char const *a, char const *b, char *buffer,
         RomeCalcError *err)
 {
-    arithmetic(a, b, buffer, err, ROME_ADD_OP);
+    arithmetic(a, b, buffer, err, ROME_CALC_ADD_OP);
 }
 
 void rome_subtract(char const *a, char const *b, char *buffer,
         RomeCalcError *err)
 {
-    arithmetic(a, b, buffer, err, ROME_SUBTRACT_OP);
+    arithmetic(a, b, buffer, err, ROME_CALC_SUBTRACT_OP);
 }
